4e8d0b075b93008f8476e5c091f4c87e5b6d9a7d
edodima13 <edodima13@libero.it>
2022-02-07 17:42:08 +0100

    avanzamento

--- myjavascript.js
+++ myjavascript.js
@@ -1,6 +1,9 @@
 //scrivere un ciclo con il for
 let str = '';
 
+
+let  nuovoDiv = document.createElement(div);
+nuovoDiv = classList
 for (let i = 1; i <= 100; i++) {
     if (i % 3 === 0){
         console.log("Fizz")
@@ -8,9 +11,8 @@ for (let i = 1; i <= 100; i++) {
     if (i %5===0){
         console.log("Bizz")
     }
+    if ((i%3===0) || (i%5===0)){
+        console.log("fizzbizz")
+    }
     console.log(i);
 }
-
-//modificare il nome dei multipli di 3 con Fizz
-//modificare il nome dei multipli di 5 con bizz
-//modificare il nome dei multipli di 3 e 5 con Fizzbizz
