fe37b6094e90df73636785d4610b7df223398d70
edodima13 <edodima13@libero.it>
2022-02-07 15:41:17 +0100

    inizio esercizio

---
+++ myjavascript.js
@@ -0,0 +1,13 @@
+//scrivere un ciclo con il for
+let str = '';
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0){
+        console.log("Fizz")
+    }
+    console.log(i);
+}
+
+//modificare il nome dei multipli di 3 con Fizz
+//modificare il nome dei multipli di 5 con bizz
+//modificare il nome dei multipli di 3 e 5 con Fizzbizz
